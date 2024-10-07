c368c5e4482d3836c7cded01189ad9275c1c88fb
Kostek <ksytnik73kwer@gmail.com>
2024-10-07 23:38:14 +0200

    refactor Navbar component to use Link instead of a tag for GitHub link

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -15,14 +15,14 @@ const Navbar = () => {
             </Link>
           </div>
           <div className="flex items-center">
-            <a
+            <Link
               href="https://github.com/yourusername/ai-word-counter"
               target="_blank"
               rel="noopener noreferrer"
               className="text-gray-500 hover:text-gray-700 dark:text-gray-400 dark:hover:text-gray-300 transition-colors"
             >
               <Github className="h-6 w-6" />
-            </a>
+            </Link>
             <div className="ml-4">
               <ModeToggle />
             </div>
